36c3bc820d113f735d19434871cae465d96310ee
moon-grp <olumidemm@gmail.com>
2020-01-16 17:28:30 +0100

    phase one done

---
+++ src/components/zipsearch.vue
@@ -0,0 +1,55 @@
+<template>
+  <ion-grid>
+    <form @submit="onSubmit">
+      <ion-col>
+        <ion-item>
+          <ion-input
+            placeholder="Enter Us based ZipCode"
+            name="zip"
+            :value="zip"
+            @input="zip = $event.target.value"
+          >
+          </ion-input>
+        </ion-item>
+      </ion-col>
+      <ion-col>
+        <ion-button type="submit" color="primary" expand="block"
+          >Find</ion-button
+        >
+      </ion-col>
+    </form>
+  </ion-grid>
+</template>
+
+<script>
+export default {
+  name: "zipsearch",
+  data() {
+    return {
+      zip: ""
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
+      const isVaildZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(this.zip);
+      if (!isVaildZip) {
+        this.showAlert();
+        this.zip=""
+      } else {
+        this.$emit("get-zip", this.zip)
+        this.zip=""
+      }
+    },
+    showAlert() {
+      return this.$ionic.alertController
+        .create({
+          header: "Error",
+          message: "Please enter valid Zipcode",
+          buttons: ["OK"]
+        })
+        .then(a => a.present());
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>ZaggaDat</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <div class="ion-padding">
+      <zipsearch v-on:get-zip="getZipInfo"/>
+    </div> 
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
+import zipsearch from "@/components/zipsearch.vue"
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    zipsearch
+  },
+  methods:{
+     showAlert() {
+      return this.$ionic.alertController
+        .create({
+          header: "Error",
+          message: "Please enter valid Us Zipcode",
+          buttons: ["OK"]
+        })
+        .then(a => a.present());
+    },
+   async getZipInfo(zip){
+      const res = await fetch(`https://api.zippopotam.us/us/${zip}`)
+      if(res.status == 404){
+        this.showAlert();
+      }
+      else{
+        this.info=await res.json()
+        console.log(this.info)
+      }
+    },
+    data() {
+      return {
+        info:null
+      }
+    },
   }
 }
 </script>
